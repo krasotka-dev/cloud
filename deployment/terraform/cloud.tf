a2c59ce61d9438b08184e3ac8bef1474da7b9bee
nsaip <nadiasaip@gmail.com>
2019-10-11 20:58:08 -0500

    .cache added

--- deployment/terraform/cloud.tf
+++ deployment/terraform/cloud.tf
@@ -1,6 +1,18 @@
+data "template_file" "exam_platform_values" {
+  template = "${file("./chart-cloud/values.yaml")}"
+  vars = {
+    domain_name = "${var.domain_name}"
+    docker_image = "${var.docker_image}"
+    docker_image_tag = "${var.docker_image_tag}"
+  }
+
 resource "helm_release" "cloud_release" {
   name       = "${var.name}"
   chart      = "${var.chart}"
   version    = "${var.version}"
-  
+
+ values = [
+    "${file("chart-cloud/values.yaml")}"
+  ]
+
 }
