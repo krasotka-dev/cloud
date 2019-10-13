d34a40aec9251a03b7adf321e1e1cd3c9c24b47c
daudmu21 <daudmuhudin@gmail.com>
2019-10-13 14:09:08 -0500

    edit

--- deployment/terraform/cloud.tf
+++ deployment/terraform/cloud.tf
@@ -1,24 +1,19 @@
-data "template_file" "chart_cloud" {
-  template = "${file("./chart-cloud/template_values.yaml")}"
+data "template_file" "cloud" {
+  template = "${file("./chart-cloud/.cache/values.yaml")}"
   vars = {
     domain_name = "${var.domain_name}"
     docker_image = "${var.docker_image}"
     docker_image_tag = "${var.docker_image_tag}"
   }
-
-  resource "local_file" "chart_cloud_values_local_file" {
-  content  = "${trimspace(data.template_file.chart_cloud_values.rendered)}"
-  filename = "./chart-cloud/.cache/values.yaml"
 }
-  
+
 resource "helm_release" "cloud_release" {
   name       = "${var.name}"
   chart      = "${var.chart}"
   version    = "${var.version}"
   namespace = "${var.namespace}"
 
-values = [
-    "{data.template_file.chart_cloud_values.rendered}"
+ values = [
+    "${file("./chart-cloud/.cache/values.yaml")}"
   ]
-}
 }
