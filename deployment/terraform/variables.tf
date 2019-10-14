e240cc4126361e50f5f2cd8aa8a273c358c9bfed
daudmu21 <daudmuhudin@gmail.com>
2019-10-14 12:30:21 -0500

    fix

--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -1,14 +1,14 @@
 variable "name" {
-  default = "cloud"
+  default = "chart-cloud"
 }
 variable "chart" {
-    default = "chart-cloud"
+    default = "./chart-cloud"
   }
 variable "version" {
     default = "6.0.1"
   }
 variable "docker_image" {
-  default = " nextcloud"
+  default = "nextcloud"
   }
 variable "docker_image_tag" {
   default = "latest"
