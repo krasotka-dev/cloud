e18cd1a5acb3da04dea15b8a7499d10e7626bf4f
cactus <cactus@cactuss-MacBook-Pro-2.local>
2019-10-18 15:13:11 -0700

    edited

--- JenkinsDeployer.groovy
+++ JenkinsDeployer.groovy
@@ -1,3 +1,3 @@
-@Library('CommonLib@master') _
-def common = new com.lib.JenkinsExternalDeployerPipeline()
+@Library('CommonLib@feature/kube-slave') _
+def common = new com.lib.JenkinsCommonDeployPipeline()
 common.runPipeline()
