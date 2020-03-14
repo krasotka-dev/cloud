6787a48c52f89b7a0195bd364a07fc905a9044da
tunji57 <tunji57@gmail.com>
2020-03-14 14:20:40 -0500

    created helm module

--- deployment/terraform/cloud.tf
+++ deployment/terraform/cloud.tf
@@ -1,23 +1,8 @@
-data "template_file" "chart_cloud_values" {
-  template = "${file("./chart-cloud/template_values.yaml")}"
-  vars = {
-    docker_image = "${var.deployment_image}"
-    deployment_endpoint = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
-
-  }
-}
-
-resource "local_file" "chart_cloud_values_local_file" {
-  content  = "${trimspace(data.template_file.chart_cloud_values.rendered)}"
-  filename = "./chart-cloud/.cache/values.yaml"
-}
-resource "helm_release" "chart_cloud" {
-  name       = "${var.name}"
-  chart      = "${var.chart}"
-  version    = "${var.version}"
-  namespace = "${var.deployment_environment}"
-
- values = [
-    "${data.template_file.chart_cloud_values.rendered}"
-  ]
+module "nextcloud_deploy" {
+ source  = "fuchicorp/chart/helm"
+  version = "0.0.3"
+  deployment_name        = "nextcloud-deployment"
+  deployment_environment = "dev"
+  deployment_endpoint    = "nextcloud.fuchicorp.com"
+  deployment_path        = "charts"
 }
